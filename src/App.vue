2206da002cbaf559197333f9a3d0a4e540898f00
LKoch <slate1233@gmail.com>
2023-01-23 15:43:48 +0100

    first steps in vite and vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const km = ref(0)
+
+</script>
+
+<template>
+  <div class="bg-gray-800 text-white flex justify-center items-center min-h-screen">
+    <input class="p-2 rounded-md w-16 text-black" type="number" v-model="km">
+    <span>--></span>
+    <div class="p-5">Miles:{{ Math.round(km / 1.609 * 100) / 100 }}</div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
